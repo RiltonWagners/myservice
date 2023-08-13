49e7f702fb481f71bec8bf700c8058d5838b02ba
Rilton Wagner <86138190+RiltonWagners@users.noreply.github.com>
2023-08-13 07:51:11 -0300

    Ajuste da função generateSlug

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Support\Str;
 
-
-if (!function_exists('generateSlug')) {
-
-    function generateSlug($text) {
-        $slug = Str::slug($text);
-        return $slug;
-    }
-    
+function generateSlug($text) {
+    $slug = Str::slug($text);
+    return $slug;
 }
